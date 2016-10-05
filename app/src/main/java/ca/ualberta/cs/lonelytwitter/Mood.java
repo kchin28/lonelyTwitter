a8061f080cce393a74834c8a5d975a8c5e3443c1
kchin28 <kgchin@ualberta.ca>
2016-10-04 19:25:14 -0600

    javadoc for mood added. was missing before

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Mood.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Mood.java
@@ -3,28 +3,44 @@ package ca.ualberta.cs.lonelytwitter;
 import java.util.Date;
 
 /**
- * Created by Kelly on 2016-09-13.
+ * Abstract class describing mood. Will be defined in each tweet.
  */
 public abstract class Mood {
     private Date date;
     //private String mood;
-
-    //constructor with default date
+/**
+ * creation date of mood will be automatically filled.
+ * */
     public Mood(){
         this.date = new Date();
     }
 
-    //constructor which takes in a user given date argument
+    /**
+     * Constructs a mood object with a given date
+     * @param date can give a date to replace the actual creation date
+     */
     public Mood(Date date){
         this.date = date;
     }
 
+    /**
+     * Abstract method string that describes the mood of the tweet
+     * @return String description of mood
+     */
     public abstract String returnCurrentMood();
 
+    /**
+     * getter method for date
+     * @return date of mood
+     */
     public Date getDate() {
         return date;
     }
 
+    /**
+     * setter method for date
+     * @param date to replace previously given date.
+     */
     public void setDate(Date date) {
         this.date = date;
     }
